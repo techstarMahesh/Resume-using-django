38b39671ff71803fa29f969c663458a62b51ee0a
Mahesh <mistermaheshsharma@gmail.com>
2022-07-05 00:25:53 +0530

    requerment.txt

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,6 +109,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
